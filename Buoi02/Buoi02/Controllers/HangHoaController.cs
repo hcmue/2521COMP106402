c7f5d0b21c61c07e45a9df863af373aa945a02cd
HIENLTH @ HCMUE <hyhien@gmail.com>
2026-01-28 08:43:08 +0700

    Demo CRUD Product

---
+++ Buoi02/Buoi02/Controllers/HangHoaController.cs
@@ -0,0 +1,72 @@
+﻿using Buoi02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi02.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        static List<HangHoa> hangHoas = new List<HangHoa>()
+        {
+            new HangHoa
+            {
+                MaHh = 1, TenHh = "IPHone 17 Pro", DonGia = 32000000, SoLuongTon = 13, Hinh = "ip17.jpg"
+            },
+            new HangHoa
+            {
+                MaHh = 2, TenHh = "IPHone 17 Pro", DonGia = 32000000, SoLuongTon = 13, Hinh = "ip17.jpg"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(hangHoas);
+        }
+
+        #region Sửa hàng hóa
+        public IActionResult Edit(int id)
+        {
+            var hangHoa = hangHoas.SingleOrDefault(p => p.MaHh == id);
+            if (hangHoa != null)
+            {
+                return View(hangHoa);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, HangHoa model)
+        {
+            // TODO
+            return View();
+        }
+        #endregion
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(HangHoa model, IFormFile UploadHinh)
+        {
+            var hangHoa = hangHoas.SingleOrDefault(p => p.MaHh == model.MaHh);
+            if (UploadHinh != null)
+            {
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(),
+                    "wwwroot", "hanghoa", UploadHinh.FileName);
+                using(var f = new FileStream(fullPath, FileMode.CreateNew))
+                {
+                    UploadHinh.CopyTo(f);
+                    model.Hinh = UploadHinh.FileName;
+                }
+            }
+            if (hangHoa == null)
+            {
+                hangHoas.Add(model);
+                return RedirectToAction("Index");
+            }
+            ViewBag.ThongBaoLoi = $"Mã {model.MaHh} đã có";
+            return View();
+        }
+    }
+}
---
+++ Buoi02/Buoi02/Models/HangHoa.cs
@@ -0,0 +1,11 @@
+﻿namespace Buoi02.Models
+{
+    public class HangHoa
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public int SoLuongTon { get; set; }
+    }
+}
