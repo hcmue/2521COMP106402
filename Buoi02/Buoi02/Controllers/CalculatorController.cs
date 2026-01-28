00d2943b2390cae16b367e44a957e98520c2ceaf
HIENLTH @ HCMUE <hyhien@gmail.com>
2026-01-28 08:12:10 +0700

    Demo Calculator

---
+++ Buoi02/Buoi02/Controllers/CalculatorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi02.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculate(double SoHang01, double SoHang02, string ToanTu)
+        {
+            double KetQua = 0;
+            switch(ToanTu)
+            {
+                case "^": KetQua = Math.Pow(SoHang01, SoHang02); break;
+                case "%": KetQua = SoHang01 % SoHang02; break;
+                case "*": KetQua = SoHang01 * SoHang02; break;
+            }
+
+
+            ViewBag.So01 = SoHang01;
+            ViewBag.So02 = SoHang02;
+            ViewBag.ToanTu = ToanTu;
+            ViewBag.KetQua = KetQua;
+            return View("Index");
+        }
+    }
+}
---
+++ Buoi02/Buoi02/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi02.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult ABC()
+        {
+            return Redirect("/Home/Privacy");
+        }
+
+        public IActionResult XYZ()
+        {
+            return RedirectToAction("Privacy", "Home");
+        }
+
+        public IActionResult MyAPI()
+        {
+            return Json(new { Time = DateTime.Now, FullName = "Admin"});
+        }
+    }
+}
