98c5bf692d1907426a646213bbcabfedc4ce222e
HIENLTH <hyhien@gmail.com>
2026-03-04 07:21:04 +0700

    Demo sync vs async

---
+++ BaiTapBuoi03/BaiTapBuoi03/Controllers/DemoController.cs
@@ -0,0 +1,33 @@
+﻿using BaiTapBuoi03.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BaiTapBuoi03.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult SyncDemo()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            Demo.FuncA();
+            Demo.FuncB();
+            Demo.FuncC();
+            sw.Stop();
+            return Content($"Chạy hết {sw.ElapsedMilliseconds} ms");
+        }
+
+        public async Task<IActionResult> SyncDemoAS()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var a = Demo.FuncAAsync();
+            var b = Demo.FuncBAsync();
+            var c = Demo.FuncCAsync();
+            //await Task.WhenAll(a, b, c);
+            await a; await b; await c;
+            sw.Stop();
+            return Content($"Chạy hết {sw.ElapsedMilliseconds} ms");
+        }
+    }
+}
---
+++ BaiTapBuoi03/BaiTapBuoi03/Models/Demo.cs
@@ -0,0 +1,40 @@
+﻿namespace BaiTapBuoi03.Models
+{
+    public class Demo
+    {
+        public static string FuncA()
+        {
+            Thread.Sleep(2000);
+            return "Hello World";
+        }
+
+        public static int FuncB()
+        {
+            Thread.Sleep(5000);
+            return new Random().Next();
+        }
+
+        public static void FuncC()
+        {
+            Thread.Sleep(3000);
+        }
+
+
+        public static async Task<string> FuncAAsync()
+        {
+            await Task.Delay(2000);
+            return "Hello World";
+        }
+
+        public static async Task<int> FuncBAsync()
+        {
+            await Task.Delay(5000);
+            return new Random().Next();
+        }
+
+        public static async Task FuncCAsync()
+        {
+            await Task.Delay(3000);
+        }
+    }
+}
