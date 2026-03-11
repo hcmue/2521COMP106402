382fa3fb77a4dfab74f0026b7e3ca45c17a90005
HIENLTH @ HCMUE <hyhien@gmail.com>
2026-03-11 08:34:22 +0700

    Demo ViewComponent

---
+++ DemoLayout/DemoLayout/Models/CategoryMenu.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoLayout.Models
+{
+    public class CategoryMenu : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            //đọc database lấy Category
+            var danhMuc = new List<Category>
+            {
+                new Category {Id=1, Name="Tủ lạnh" },
+                new Category {Id=2, Name="Điều hòa" }
+            };
+            return View(danhMuc);
+        }
+    }
+}
