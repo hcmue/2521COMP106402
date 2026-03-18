f119c6aa266cec06231d8ec2197767534c1cdfa4
HIENLTH @ HCMUE <hyhien@gmail.com>
2026-03-18 07:50:24 +0700

    Define Entity Model

---
+++ DemoDb/DemoDb/Data/HangHoa.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoDb.Data
+{
+    [Table("Product")]
+    public class HangHoa
+    {
+        [Key]
+        public int MaHH { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string TenHH { get; set; }
+        public double DonGia { get; set; }
+        public int SoLuong { get; set; }
+        [MaxLength(150)]
+        public string? Hinh { get; set; }
+        public int MaLoai { get; set; }
+
+        [ForeignKey("MaLoai")]
+        public Loai Loai { get; set; }
+    }
+}
---
+++ DemoDb/DemoDb/Data/Loai.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoDb.Data
+{
+    [Table("Category")]
+    public class Loai
+    {
+        [Key]
+        public int MaLoai { get; set; }
+        [MaxLength(50)]
+        public string TenLoai { get; set; }
+        public string MoTa { get; set; }
+        [MaxLength(150)]
+        public string? Hinh { get; set; }
+
+        public ICollection<HangHoa> HangHoas { get; set; } = new List<HangHoa>();
+
+    }
+}
